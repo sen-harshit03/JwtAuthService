7d412197a8fba09795b103f5fa2ce3555026b5b1
sen-harshit03 <harshitsen03@gmail.com>
2024-01-20 02:34:25 +0530

    Clean up imports

--- src/main/java/com/example/authservice/security/filter/JwtAuthFilter.java
+++ src/main/java/com/example/authservice/security/filter/JwtAuthFilter.java
@@ -1,21 +1,15 @@
 package com.example.authservice.security.filter;
 
-import com.example.authservice.exceptions.InvalidTokenException;
 import com.example.authservice.service.CustomUserDetailsService;
 import com.example.authservice.utils.JwtUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.security.web.csrf.InvalidCsrfTokenException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
